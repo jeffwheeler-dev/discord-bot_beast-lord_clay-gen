ec3aae9490addfcdb8d540bef10ead7fd25ff492
Jeff Wheeler <jeffwheeler.dev@gmail.com>
2024-03-14 18:31:27 -0400

    models have been simplified

--- src/database/models/Setting.js
+++ src/database/models/Setting.js
@@ -12,20 +12,11 @@ const settingSchema = new mongoose.Schema({
     type: String,
     required: false, // Optional, as not all servers might designate a mod role
   },
-  channelId: {
+  channelID: {
     type: String, 
     required: true,
     unique: true // Ensures one primary channel per server's settings
-  },
-  // You can keep the allianceId if you want settings to be tied to specific alliances
-  // and if alliances are distinct entities within servers.
-  // Otherwise, serverID alone might suffice for server-wide settings.
-  allianceId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: false, // Optional, depends on whether you're using alliances as separate entities
-    ref: 'Alliance'
-  },
-  // Add other settings specific to servers or alliances
+  }
 });
 
 const Setting = mongoose.model('Setting', settingSchema);
--- src/database/models/Tower.js
+++ src/database/models/Tower.js
@@ -1,28 +1,25 @@
 const mongoose = require('mongoose');
 
 const towerSchema = new mongoose.Schema({
-    channelId: {
-        type: String,
-        required: true,
-    },
-    allianceName: {
-        type: String,
-        required: true,
-    },
-    isActive: {
-        type: Boolean,
-        default: true,
-    },
-    location: {
-        type: String,
-        required: true,
-    },
-    placedAt: {
-        type: Date,
-        default: Date.now,
-    },
-    // Add additional fields as needed for your game's mechanics
-    // For example, towerType, healthPoints, attackPower, etc.
+  allianceId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Alliance',
+    required: true,
+  },
+  allianceName: {
+    type: String,
+    required: true,
+  },
+  homeTowerCount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  awayTowerCount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
 });
 
 const Tower = mongoose.model('Tower', towerSchema);
